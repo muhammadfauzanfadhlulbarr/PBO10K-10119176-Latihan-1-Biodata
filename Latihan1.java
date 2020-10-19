e3ca4bb54b5938833666a60c281ae88b137fa6cd
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:29:46 +0700

    first commit

---
+++ Latihan1.java
@@ -0,0 +1,21 @@
+/**
+ * Latihan1
+ */
+public class Latihan1{
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA LENGKAP" + "\n" + "------------------");
+        System.out.println("NAMA\t\t: Muhammad Fauzan Fadhlulbarr");
+        System.out.println("ALAMAT\t\t: Jl.KH Ruhiat");
+        System.out.println("TEMPAT LAHIR\t: Tasikmalaya");
+        System.out.println("TANGGAL LAHIR\t: 12 Agustus");
+        System.out.println("------------------");
+        System.out.println("HOBBY\t\t: Reading");
+        System.out.println("MAKES\t\t: Chicken");
+        System.out.println("MIKES\t\t: Pop Ice");
+
+
+    }
+
+    
+}
